ab5d9c09f3beba731c66f9eecf22040e67c7ef42
jcow <j_cowan06@hotmail.com>
2014-01-27 16:12:54 -0700

    some refactoring with George

--- KNN/knn.jl
+++ KNN/knn.jl
@@ -1,6 +1,11 @@
-function dist(point, data)
-	d = zeros(FloatingPoint, length(data[1:end,1]))
+# determine a one-dimensional array of indicies by their distance to the passed in point
+function old_dist(point, data)
+        # creates a matrix of floating point zeros of the data size
+        d = zeros(FloatingPoint, size(data,1))
+
+        # loop through the entire 0 matrix
 	for i = 1:length(d)
+                # loop through the columns of the data point and the
 		temp_d = 0
 		for j = 1:length(data[i, 1:end])
 			temp_d += (point[j]-data[i,j])^2
@@ -12,7 +17,17 @@ function dist(point, data)
 		
 	end
 	
+        #return an array that's sorted by their indicies
 	return sortperm(d)
+end
+
+# determine a one-dimensional array of indicies by their distance to the passed in point
+function dist(point, data)
+    summedSquares = sum(broadcast((c,d)->(c-d)^2, point, data),2)
+    dists = sqrt(convert(Array{FloatingPoint,2}, summedSquares))
+    println(typeof(dists))
+    dists = reshape(dists,2)
+    return sortperm(dists)
 end
 
 fruitCSV = readcsv("fruit.csv")
@@ -24,7 +39,8 @@ pointClasses = pointCSV[2:end, end]
 
 k = 10
 accuracyCount = 0
-for i = 1:length(pointData[1:end,1])
+# loop over the test data
+for i = 1:size(pointData,1)
 	indicies = dist(pointData[i,1:end], fruitData)
 	
 	maxClasses = Dict()
