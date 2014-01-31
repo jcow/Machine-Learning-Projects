121f56919620a877bc6f5b0ad16f22cf87de46b7
George Lesica <glesica@gmail.com>
2014-01-31 15:41:11 -0700

    Implement sexy KNN function and test code.

--- KNN/knn.jl
+++ KNN/knn.jl
@@ -1,77 +1,46 @@
-# determine a one-dimensional array of indicies by their distance to the passed in point
-function old_dist(point, data)
-        # creates a matrix of floating point zeros of the data size
-        d = zeros(FloatingPoint, size(data,1))
+using StatsBase
 
-        # loop through the entire 0 matrix
-	for i = 1:length(d)
-                # loop through the columns of the data point and the
-		temp_d = 0
-		for j = 1:length(data[i, 1:end])
-			temp_d += (point[j]-data[i,j])^2
-		end
-		
-		if temp_d != 0
-			d[i] = sqrt(temp_d)
-		end
-		
-	end
-	
-        #return an array that's sorted by their indicies
-	return sortperm(d)
+function knn(k, train, classes, obs)
+    nearest = sortperm(vec(sqrt(sum(broadcast((a, b) -> (a-b)^2, obs, train), 2))))[1:k]
+    return mode(classes[nearest])
 end
 
-# determine a one-dimensional array of indicies by their distance to the passed in point
-function dist(point, data)
-    summedSquares = sum(broadcast((c,d)->(c-d)^2, point, data),2)
-    dists = sqrt(convert(Array{FloatingPoint,2}, summedSquares))
-    println(typeof(dists))
-    dists = reshape(dists,2)
-    return sortperm(dists)
+fruit_data, fruit_headers = readdlm("fruit.csv", ',',  has_header=true)
+training_data = convert(Array{Float64, 2}, fruit_data[1:end, 1:end-1])
+classes = fruit_data[1:end, end]
+
+# Classify each test instance and check against the given class
+train_raw = readcsv("fruit.csv")[2:end, 1:end]
+train_data = convert(Array{Float64, 2}, train_raw[1:end, 1:end-1])
+train_classes = train_raw[1:end, end]
+
+test_raw = readcsv("fruit.csv")[2:end, 1:end]
+test_data = convert(Array{Float64, 2}, test_raw[1:end, 1:end-1])
+test_classes = test_raw[1:end, end]
+
+verbose = false
+println("k\taccuracy")
+for k=[1 5 10 20 100]
+    successes = 0
+    failures = 0
+    for i=1:100
+        guess = knn(k, train_data, train_classes, test_data[i, 1:end])
+        actual = test_classes[i]
+        if guess == actual
+            successes += 1
+            if verbose
+                print("SUCCESS: ")
+            end
+        else
+            failures += 1
+            if verbose
+                print("FAILURE: ")
+            end
+        end
+        if verbose
+            println("Guess: $guess Actual: $actual")
+        end
+    end
+    accuracy = successes / (successes + failures)
+    println("$k\t$accuracy")
 end
-
-fruitCSV = readcsv("fruit.csv")
-pointCSV = readcsv("testFruit.csv")
-fruitData = fruitCSV[2:end, 1:end-1]
-pointData = pointCSV[2:end, 1:end-1]
-fruitClasses = fruitCSV[2:end, end]
-pointClasses = pointCSV[2:end, end]
-
-k = 10
-accuracyCount = 0
-# loop over the test data
-for i = 1:size(pointData,1)
-	indicies = dist(pointData[i,1:end], fruitData)
-	
-	maxClasses = Dict()
-	for j = 1:k
-		index = indicies[j]
-		class = fruitClasses[index]
-		
-		if haskey(maxClasses, class) == false
-			maxClasses[class] = 1
-		else
-			maxClasses[class] = maxClasses[class] + 1
-		end
-	end
-	
-	maxCount = -1
-	maxClass = ""
-	for j = keys(maxClasses)
-		if maxClasses[j] > maxCount
-			maxCount = maxClasses[j]
-			maxClass = j
-		end
-	end
-	
-	if maxClass == pointClasses[i]
-		accuracyCount = accuracyCount + 1
-	end
-end
-
-accuracy = accuracyCount/length(pointData[1:end,1])
-println("Hits: $accuracyCount")
-println("Accuracy: $accuracy")
-
-
-
