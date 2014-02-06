fc0f3946cf0e17f7bc301948c96f281bb1cfacdc
George Lesica <glesica@gmail.com>
2014-02-06 12:36:04 -0700

    Add comments for Doug.

--- KNN/knn.jl
+++ KNN/knn.jl
@@ -1,4 +1,6 @@
 function knn_normalize{T}(D::Matrix{T}, mx::Vector{T}, mn::Vector{T})
+    # Map over the rows and weight them based on the max and min values given.
+    # Note that the actual function evaluation is vectorized.
     return mapslices(x -> (x - mn) ./ (mx - mn), D, 2)
 end
 
@@ -7,12 +9,18 @@ function knn_normalize{T}(D::Vector{T}, mx::Vector{T}, mn::Vector{T})
 end
 
 function knn_maxmin{T}(D::Matrix{T})
+    # Map over the columns and find the max and min of each. Return the max and
+    # min vectors as a tuple.
     mx = vec(mapslices(maximum, D, 1))
     mn = vec(mapslices(minimum, D, 1))
     return mx, mn
 end
 
 function knn_dists{T}(train::Matrix{T}, obs::Vector{T})
+    # This is a bit dense (concise). Basically, map the squred difference
+    # function, in a vectorized manner, down the training set, reusing the
+    # instance vector each time. Then sum the distances in each dimension and
+    # take the square root. Turn the result into a vector.
     return vec(sqrt(sum(broadcast((a, b) -> (a-b).^2, transpose(obs), train), 2)))
 end
 
@@ -21,6 +29,8 @@ function knn_weights{T}(dists::Vector{T})
 end
 
 function knn_tally{T, J}(votes::Vector{T}, classes::Vector{J})
+    # Create a running tally of the votes for each class, adding classes to the
+    # dictionary as they are discovered.
     tallies = Dict{J, T}()
     for (c, v) = zip(classes, votes)
         if ! haskey(tallies, c)
@@ -28,6 +38,7 @@ function knn_tally{T, J}(votes::Vector{T}, classes::Vector{J})
         end
         tallies[c] += v
     end
+    # Iterate through the dictionary and find the class with the most votes.
     winner::J = classes[1]
     winner_votes::T = votes[1]
     for (c, v) = zip(keys(tallies), values(tallies))
