e09cfe75652e4dd1d2c5634980d3d90e07beb3c6
jcow <j_cowan06@hotmail.com>
2014-02-16 00:03:38 -0700

    split up functionality and added tests

--- Naive Bayes/dev/jason/nb.jl
+++ Naive Bayes/dev/jason/nb.jl
@@ -1,27 +1,25 @@
-function max_min{T}(D::Array{T, 2})
+function nb_max_min{T}(D::Array{T, 2})
     mx = vec(mapslices(maximum, D, 1))
     mn = vec(mapslices(minimum, D, 1))
     return mx, mn
 end
 
+
 # normalize over the D by row with the min max of each column in mx mn
-function zero_one_normalize{T}(D::Array{T, 2}, mx::Array{T, 1}, mn::Array{T, 1})
+function nb_zero_one_normalize{T}(D::Array{T, 2}, mx::Array{T, 1}, mn::Array{T, 1})
     return mapslices(x -> (x - mn) ./ (mx - mn), D, 2)
 end
 
-function map_to_bin(value, bin_count)
-  println("***")
-  println(value)
-  println(bin_count)
-  println("***")
+function nb_map_to_bin(value, bin_count)
     return ceil(value*(bin_count-1)+1)
 end
 
-function percentage_correct{T}(guesses::Array{T,1}, actual::Array{T,1})
+
+function nb_percentage_correct{T}(guesses::Array{T,1}, actual::Array{T,1})
   successes = 0
   failures = 0
   for i = 1:length(guesses)
-        if guesses[i] == test_classes[i]
+        if guesses[i] == actual[i]
             successes += 1
         else
             failures += 1
@@ -30,61 +28,57 @@ function percentage_correct{T}(guesses::Array{T,1}, actual::Array{T,1})
   return successes / (successes + failures)
 end
 
-# read the training csv file and break it into the data and classes
-train_raw = readcsv("data/fruit.csv")[2:end, 1:end]
-train_data = convert(Array{Float64, 2}, train_raw[1:end, 1:end-1])
-train_classes = train_raw[1:end, end]
 
-(train_mx, train_mn) = max_min(train_data)
-train_norm = zero_one_normalize(train_data, train_mx, train_mn)
-
-# read the test csv file and break it into the data and classes
-test_raw = readcsv("data/testFruit.csv")[2:end, 1:end]
-test_data = convert(Array{Float64, 2}, test_raw[1:end, 1:end-1])
-test_classes = convert(Array{ASCIIString, 1}, test_raw[1:end, end])
-
-(test_mx, test_mn) = max_min(train_data)
-test_norm = zero_one_normalize(test_data, test_mx, test_mn)
+function nb_find_max_from_dict{T,D}(estimates::Dict{T,D})
+  mx = ("", 0)
+  for (class, value) = zip(keys(estimates), values(estimates))
+    if value > mx[2]
+      mx = (class, value)
+    end
+  end
+  return mx
+end
 
 
+function nb_make_universe{T,S,D}(data::Array{T,2}, classes::Array{S, 1}, bin_count::D)
 
-bin_count = convert(Int64, 20)
-train_dims = size(train_norm)
+  data_dims = size(data)
 
-#####
-# Make the universe
-#####
-universe_totals = Dict{ASCIIString, Int64}()
-universe_dimension_counts = zeros(Float64, train_dims[2], bin_count)
-universe_counts = Dict{ASCIIString, Array{Float64,2}}()
+  universe_counts = Dict{ASCIIString, Array{T,2}}()
+  universe_dimension_counts = zeros(T, data_dims[2], bin_count)
+  universe_totals = Dict{ASCIIString, D}()
 
+  for i = 1:data_dims[1]
+    observation  = data[i,:]
+    class = classes[i]
 
-for i = 1:train_dims[1]
-	observation  = train_norm[i,:]
-	class = train_classes[i]
+    # make the bins
+    if !haskey(universe_counts, class)
+      universe_counts[class] = zeros(Float64, data_dims[2], bin_count)
+    end
 
-  # make the bins
-  if !haskey(universe_counts, class)
-    universe_counts[class] = zeros(Float64, train_dims[2], bin_count)
+    # add to the universe totals
+    if !haskey(universe_totals, class)
+      universe_totals[class] = 0
+    end
+
+    #add to the bin counts
+    for j = 1:length(observation)
+      bin_index = nb_map_to_bin(observation[j], bin_count)
+      universe_counts[class][j, bin_index] += 1
+      universe_dimension_counts[j, bin_index] += 1
+    end
+
+    # add to the bin number at the specific dimension
+    universe_totals[class] += 1
   end
 
-  # add to the universe totals
-  if !haskey(universe_totals, class)
-    universe_totals[class] = 0
-  end
+  return (universe_counts, universe_dimension_counts, universe_totals)
+end
 
 
-  #add to the bin counts
-  for j = 1:length(observation)
-    bin_index = map_to_bin(observation[j], bin_count)
-
-    universe_counts[class][j, bin_index] += 1
-    universe_dimension_counts[j, bin_index] += 1
-  end
-
-  # add to the bin number at the specific dimension
-  universe_totals[class] += 1
-
+function nb_m_estimate(n, nc, N)
+    return (n + 1) / (nc + N)
 end
 
 
@@ -100,48 +94,46 @@ function test_point{T,D,S}(
   for (class, dims_n_bin) = zip(keys(universe_counts), values(universe_counts))
     estimates[class] = 1
     for i = 1:length(test_point)
-      matching_bin_index = map_to_bin(test_point[i], bin_count)
+      matching_bin_index = nb_map_to_bin(test_point[i], bin_count)
+      
       m_estimate = 1
-      println(i)
-      println(matching_bin_index)
-      println("----")
-      if matching_bin_index < 1 || matching_bin_index > bin_count
-        m_estimate = 1
-      else
-        m_estimate = (
-            (dims_n_bin[i,matching_bin_index] + 1)
-            / (universe_dimension_counts[i, matching_bin_index] + universe_totals[class])
-        )
+      if (1 <= m_estimate && m_estimate <= matching_bin_index)
+        m_estimate = nb_m_estimate(
+                  dims_n_bin[i,matching_bin_index],
+                  universe_dimension_counts[i, matching_bin_index],
+                  universe_totals[class]
+              )
       end
+
       estimates[class] *= m_estimate
+      
     end
   end
 
-  max_val = 0
-  max_class = ""
-  for (class, value) = zip(keys(estimates), values(estimates))
-    if value > max_val
-      max_val = value
-      max_class = class
-    end
+  max_val = nb_find_max_from_dict(estimates)
+  return max_val[1]
+end
+
+
+function nb{T,S}(
+  train_data::Array{T,2}, 
+  train_classes::Array{S,1}, 
+  test_data::Array{T,2}, 
+  test_classes::Array{S,1}, 
+  bin_count)
+
+  (universe_counts, universe_dimension_counts, universe_totals) = nb_make_universe(train_data, train_classes, bin_count)
+
+  test_dims = size(test_data)
+
+  guesses = Array(ASCIIString, test_dims[1])
+  for i = 1:test_dims[1]
+    guesses[i] = test_point(universe_counts, universe_totals, universe_dimension_counts, vec(test_data[i,:]), bin_count)
   end
 
-  return max_class
+  return nb_percentage_correct(guesses, test_classes)
 end
 
-#####
-# Do some testing against the universe
-#####
 
-#guess_class = test_point(universe_counts, universe_totals, universe_dimension_counts, vec(test_norm[1,:]), bin_count)
 
-test_dims = size(test_norm)
-guesses = Array(ASCIIString, test_dims[1])
-for i = 1:test_dims[1]
-  println("I = $i")
-  guesses[i] = test_point(universe_counts, universe_totals, universe_dimension_counts, vec(test_norm[i,:]), bin_count)
-end
-
-println("accuracy")
-println(percentage_correct(guesses, test_classes))
 
