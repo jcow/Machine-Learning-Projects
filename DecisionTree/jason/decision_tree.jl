019053baa799fd605126a1085ad2d575d3729b32
jcow <j_cowan06@hotmail.com>
2014-02-23 15:36:24 -0700

    added information gain function

--- DecisionTree/jason/decision_tree.jl
+++ DecisionTree/jason/decision_tree.jl
@@ -8,7 +8,7 @@ function dt_sum_from_dict{T,J}(d::Dict{T,J})
 	return sum
 end
 
-# gets the entropy for a dictionary of counts
+# gets the entropy for a single dictionary of counts
 function dt_entropy{T, J}(counts::Dict{T,J})
 	entropy = 0
 	sum_of_dict = dt_sum_from_dict(counts)
@@ -33,6 +33,19 @@ function dt_entropy_from_counts(counts)
 	return entropy
 end
 
+function dt_count_attr{T}(attr::Array{T,1})
+	counts = Dict()
+	for i = 1:length(attr)
+		if !haskey(counts, attr[i])
+			counts[attr[i]] = 0
+		end
+
+		counts[attr[i]] += 1
+	end
+
+	return counts
+end
+
 # gets and returns a dict(dict(int)) of the counts for each preditor and target
 function dt_count_attr{T}(predictor_attribute::Array{T,1}, target::Array{T,1})
 	predict_attrs = Dict()
@@ -49,3 +62,9 @@ function dt_count_attr{T}(predictor_attribute::Array{T,1}, target::Array{T,1})
 	return predict_attrs
 end
 
+# information gain for a specific attribute
+function gain{T}(predictor_attribute::Array{T,1}, target::Array{T,1})
+	attribute_counts = dt_count_attr(predictor_attribute, target)
+	target_counts = dt_count_attr(target)
+	return dt_entropy(target_counts) - dt_entropy_from_counts(attribute_counts)
+end
--- DecisionTree/jason/test.jl
+++ DecisionTree/jason/test.jl
@@ -14,7 +14,7 @@ const entropy2 = dt_entropy(["a"=>5, "b"=>5])
 @test entropy2 == 1
 
 
-# test the attribute entropy function
+# test the attribute count function
 const attr_counts = dt_count_attr(vec(predictors[:,1]), target)
 @test attr_counts["rainy"]["yes"] == 2
 @test attr_counts["rainy"]["no"] == 3
@@ -23,6 +23,11 @@ const attr_counts = dt_count_attr(vec(predictors[:,1]), target)
 @test attr_counts["overcast"]["yes"] == 4
 @test attr_counts["overcast"]["no"] == 0
 
+# test the attribute count function with the single array
+const single_array_count = dt_count_attr(["a", "a", "b", "b", "c"])
+@test single_array_count["a"] == 2
+@test single_array_count["b"] == 2
+@test single_array_count["c"] == 1
 
 # test the counts for entropy for a single class
 const entropy_from_counts = dt_entropy_from_counts([
@@ -31,3 +36,12 @@ const entropy_from_counts = dt_entropy_from_counts([
 	"rainy"=>["yes"=>2, "no"=>3]
 ])
 @test_approx_eq_eps entropy_from_counts 0.693 0.001
+
+
+# test the information gain
+const info_gain1 = gain(vec(predictors[:,1]), target)
+@test_approx_eq_eps info_gain1 0.246 0.001
+
+const info_gain2 = gain(vec(predictors[:,2]), target)
+@test_approx_eq_eps info_gain1 0.029 0.001
+
